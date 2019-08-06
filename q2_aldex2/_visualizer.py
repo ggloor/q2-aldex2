2ededa1fda8ddbb12c866a9bc1dee21fabfbfc76
Daniel Giguere <dgiguer@uwo.ca>
2019-08-06 12:44:20 -0400

    effect plot update
    - effect plot is now coloured by significance (assumes t test was done). options for this will need to be updated later and added as paramters

--- q2_aldex2/_visualizer.py
+++ q2_aldex2/_visualizer.py
@@ -15,19 +15,36 @@ TEMPLATES = pkg_resources.resource_filename('q2_aldex2', 'assets')
 def effect_plot(output_dir: str, table: pd.DataFrame, type: str) -> None:
 
     #fun
-        # effect plot
-        plt.scatter(x="diff.win", y="diff.btw", data=table, color="grey", s = 10)
+        # base effect plot to build on
+        plt.scatter(x="diff.win", y="diff.btw", data=table, color="grey", s = 6)
 
         # get maximum value of diff.btw to dynamically lengththen effect size line
         btw_max = table['diff.win'].max()
 
+        # get points that pass rare threshold
+        # change rare to parameter eventually
+        rare = 0
+        rare_points = table["rab.all"] < rare
+
+        # sig cutoff for expected benjamini hochberg correct p value of welchs t test
+        # change cutoff to parameter eventually
+        cutoff = 0.1
+        called = table["we.eBH"] <= cutoff
+
         # plot positive and negative effect size line
-        plt.plot([0, btw_max], [0,btw_max], 'k--', [0,btw_max], [0,-btw_max], 'k--', linewidth=1)
+        plt.plot([0, btw_max], [0,btw_max], color = "grey", linestyle='dashed', linewidth=1)
+        plt.plot([0,btw_max], [0,-btw_max], color = "grey", linestyle='dashed', linewidth=1)
+
+        # colour for rare points
+        plt.scatter(x="diff.win", y="diff.btw", data=table[rare_points], color="black", s = 6)
+
+        # colour for significant points
+        plt.scatter(x="diff.win", y="diff.btw", data=table[called], color="red", s = 6)
 
         # change titles and labels
         plt.suptitle('Effect plot')
-        plt.xlabel('Difference within')
-        plt.ylabel('Difference between')
+        plt.xlabel('Median Log2 difference within groups')
+        plt.ylabel('Median Log2 difference between groups')
 
         img_fp = os.path.join(output_dir, "effect_plot.png")
         plt.savefig(img_fp)
